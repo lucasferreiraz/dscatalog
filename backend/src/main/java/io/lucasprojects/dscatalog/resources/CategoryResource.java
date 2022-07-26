3a5662608aa6c69e92fd01608f447d8eedfd7c72
Lucas Ferreira <ferreiralucas090@gmail.com>
2022-07-26 19:19:59 -0300

    wip: CategoryResource post method

--- backend/src/main/java/io/lucasprojects/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/io/lucasprojects/dscatalog/resources/CategoryResource.java
@@ -1,13 +1,17 @@
 package io.lucasprojects.dscatalog.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import io.lucasprojects.dscatalog.dto.CategoryDTO;
 import io.lucasprojects.dscatalog.services.CategoryService;
@@ -32,5 +36,18 @@ public class CategoryResource {
         CategoryDTO dto = categoryService.findById(id);
         return ResponseEntity.ok().body(dto);
     } 
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+
+        dto = categoryService.insert(dto);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                                             .path("/{id}")
+                                             .buildAndExpand(dto.getId())
+                                             .toUri();
+
+        return ResponseEntity.created(uri).body(dto);
+    }
 
 }
--- backend/src/main/java/io/lucasprojects/dscatalog/services/CategoryService.java
+++ backend/src/main/java/io/lucasprojects/dscatalog/services/CategoryService.java
@@ -29,10 +29,22 @@ public class CategoryService {
         return listDTO;
     }
 
+    @Transactional(readOnly = true)
     public CategoryDTO findById(Long id){
         Optional<Category> obj = categoryRepository.findById(id);
         Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found or not exist."));
         return new CategoryDTO(entity);
+    }
+
+    @Transactional
+    public CategoryDTO insert(CategoryDTO dto){
+        Category entity = new Category();
+        entity.setName(dto.getName());
+
+        entity = categoryRepository.save(entity);
+
+        return new CategoryDTO(entity);
+
     }
 
 }
