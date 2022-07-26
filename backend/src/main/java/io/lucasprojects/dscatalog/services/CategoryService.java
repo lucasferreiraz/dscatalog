c6a84f2da27870698c69b12e8084f7341c14dd2f
Lucas Ferreira <ferreiralucas090@gmail.com>
2022-07-26 17:11:52 -0300

    feat: exception handling

--- backend/src/main/java/io/lucasprojects/dscatalog/services/CategoryService.java
+++ backend/src/main/java/io/lucasprojects/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package io.lucasprojects.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import io.lucasprojects.dscatalog.dto.CategoryDTO;
 import io.lucasprojects.dscatalog.entities.Category;
 import io.lucasprojects.dscatalog.repositories.CategoryRepository;
+import io.lucasprojects.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -24,11 +26,13 @@ public class CategoryService {
                                         .map(category -> new CategoryDTO(category))
                                         .collect(Collectors.toList());
 
-        /* for(Category category : list){
-            listDTO.add(new CategoryDTO(category));
-        } */
-
         return listDTO;
+    }
+
+    public CategoryDTO findById(Long id){
+        Optional<Category> obj = categoryRepository.findById(id);
+        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found or not exist."));
+        return new CategoryDTO(entity);
     }
 
 }
---
+++ backend/src/main/java/io/lucasprojects/dscatalog/services/exceptions/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package io.lucasprojects.dscatalog.services.exceptions;
+
+public class EntityNotFoundException extends RuntimeException {
+
+    public EntityNotFoundException(String msg){
+        super(msg);
+    }
+    
+}   
